11ead148e80b8745507982decebee8ff5b7173b5
Derek <dberb789@gmail.com>
2025-06-01 18:04:33 +0800

    feat: implement AuthPage component layout

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,12 @@
+export default function AuthPage() {
+    return (
+        <div className="grid grid-cols-2 gap-4 p-4 min-h-screen">
+            <div className="flex items-center justify-center">
+                Banner
+            </div>
+            <div className="flex items-center justify-center">
+                Auth
+            </div>
+        </div>
+    )
+}
