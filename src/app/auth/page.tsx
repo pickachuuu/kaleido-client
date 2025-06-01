93096cc6f50dd209b5b5cc370599c36c335b27ff
Derek <dberb789@gmail.com>
2025-06-01 19:11:05 +0800

    feat: replace placeholder elements with Banner and AuthForm components in AuthPage

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,12 +1,11 @@
+import AuthForm from "@/components/features/AuthForm"
+import Banner from "@/components/features/Banner"
+
 export default function AuthPage() {
     return (
         <div className="grid grid-cols-2 gap-4 p-4 min-h-screen">
-            <div className="flex items-center justify-center">
-                Banner
-            </div>
-            <div className="flex items-center justify-center">
-                Auth
-            </div>
+            <Banner />
+            <AuthForm />
         </div>
     )
 }
