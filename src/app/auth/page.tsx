37efdba5133cee05f71a9322e69a00b21694c585
Derek <dberb789@gmail.com>
2025-06-02 22:20:52 +0800

    feat: enhance grid layout for responsive design in AuthPage component

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,7 +3,7 @@ import Banner from "@/components/features/Banner"
 
 export default function AuthPage() {
     return (
-        <div className="grid grid-cols-2 gap-4 p-4 min-h-screen">
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 p-4 min-h-screen">
             <Banner />
             <AuthForm />
         </div>
