6208d9cecd2b85fce40e49e04a06a7348cac63a4
Derek <dberb789@gmail.com>
2025-06-02 21:29:29 +0800

    feat: implement AuthForm component with Google and Github sign-in options

--- src/components/features/AuthForm.tsx
+++ src/components/features/AuthForm.tsx
@@ -1,7 +1,59 @@
+import Card from "@/components/ui/Card";
+import { Button } from "@/components/ui/Button";
+
 export default function AuthForm() {
     return (
         <div className="flex items-center justify-center">
-            Auth
+            <Card className="w-96">
+                
+                <Card.Header className="text-bold text-2xl">
+                    KALEIDO.
+                </Card.Header>
+
+                <Card.Content className="h-96">
+                    <div className="col col-span-1">
+                        <div>
+                            <Button className="w-full my-2">
+                                Sign in with Google
+                            </Button>
+                        </div>
+                        <div>
+                            <Button className="w-full my-2">
+                                Sign in with Github
+                            </Button>
+                        </div>
+
+                        {/* divider */}
+                        <div className="flex items-center gap-4 text-gray-500 text-sm my-6">
+                            <hr className="flex-grow border-t border-gray-500" />
+                            <span className="text-white">OR</span>
+                            <hr className="flex-grow border-t border-gray-500" />
+                        </div>
+
+                        <div>
+                            <Button className="w-full my-2" variant="secondary">
+                                Create an account
+                            </Button>
+                        </div>
+                        
+                        <div className="text-gray-500 text-sm mt-4">
+                            <p>
+                                By signing up, you agree to the Terms of Service and Privacy Policy, including Cookie Use.
+                            </p>
+                        </div>
+
+                        <div className="text-bold text-white my-12">
+                            Already have an account?
+                            <Button className="w-full my-2" variant="outline">
+                                Sign in
+                            </Button>
+                        </div>
+                    </div>
+                
+
+                </Card.Content>
+            
+            </Card>
         </div>
     )
 }
