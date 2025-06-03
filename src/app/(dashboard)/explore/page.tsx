1db9a4c389036fe105313c9cf159bed01af91fa1
Derek <dberb789@gmail.com>
2025-06-04 02:51:44 +0800

    style: update border color in ExplorePage component for consistency

--- src/app/(dashboard)/explore/page.tsx
+++ src/app/(dashboard)/explore/page.tsx
@@ -1,6 +1,6 @@
 export default function ExplorePage() {
     return (
-        <div className="border-x-1 h-screen max-w-3xl border-gray-700">
+        <div className="border-x-1 h-screen max-w-3xl border-zinc-700">
             Explore
         </div>
     )
