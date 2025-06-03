8b6f29abb78f72f48e55c8a5f74c7a16ebaf61dd
Derek <dberb789@gmail.com>
2025-06-04 04:14:29 +0800

    refactor: improve home page layout

--- src/app/(dashboard)/home/layout.tsx
+++ src/app/(dashboard)/home/layout.tsx
@@ -5,21 +5,18 @@ export default function HomeLayout({
 }) {
 
     return (
-        <div className="">
-            <div className="border-x-1 h-screen max-w-3xl border-zinc-700">
-                <div className="grid grid-cols-2">
-                    <div className=" border-b-1 border-zinc-700 p-3 text-center hover:bg-zinc-900">
-                        For you
-                    </div>
-                    <div className="border-b-1 border-zinc-700 p-3 text-center hover:bg-zinc-900">
-                        Following
-                    </div>
+        <div className="border-r-1 h-screen w-2xl border-zinc-700">
+            <div className="grid grid-cols-2">
+                <div className=" border-b-1 border-zinc-700 p-3 text-center hover:bg-zinc-900">
+                    For you
                 </div>
+                <div className="border-b-1 border-zinc-700 p-3 text-center hover:bg-zinc-900">
+                    Following
+                </div>
+            </div>
 
-                {/* Home Page Feed */}
-                {children}
-
-            </div>  
-        </div>
+            {/* Home Page Feed */}
+            {children}
+        </div>  
     )
 }
