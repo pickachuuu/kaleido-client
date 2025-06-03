38aa34f08b63999d37bacedd9414cff233073936
Derek <dberb789@gmail.com>
2025-06-04 02:52:00 +0800

    style: update border color in MessagesPage component for consistency

--- src/app/(dashboard)/messages/page.tsx
+++ src/app/(dashboard)/messages/page.tsx
@@ -1,6 +1,6 @@
 export default function MessagesPage() {
     return (
-        <div className="border-x-1 h-screen max-w-3xl border-gray-700">
+        <div className="border-x-1 h-screen max-w-3xl border-zinc-700">
             Messages
         </div>
     )
