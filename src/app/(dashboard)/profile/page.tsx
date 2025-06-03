36be6869196fce1177451abac4b1c65efa553d61
Derek <dberb789@gmail.com>
2025-06-04 02:53:12 +0800

    style: update border color in ProfilePage component for consistency

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -1,6 +1,6 @@
 export default function ProfilePage() {
     return (
-        <div className="border-x-1 h-screen max-w-3xl border-gray-700">
+        <div className="border-x-1 h-screen max-w-3xl border-zinc-700">
             Profile Page
         </div>
     )
