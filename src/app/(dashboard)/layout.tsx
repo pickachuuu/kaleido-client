4bbb67e147e81e7699141b7bff90731c95e8a448
Derek <dberb789@gmail.com>
2025-06-04 04:14:51 +0800

    refactor: restructure DashboardLayout for improved layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -6,13 +6,15 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="h-screen flex">
-        <Sidebar />
+    <div className="flex justify-center w-full">
+      <div className="flex max-w-7xl w-full">
+        <aside className="w-64 h-screen sticky top-0 shrink-0 border-r border-zinc-700">
+          <Sidebar />
+        </aside>
         <main className="flex-1 min-h-screen">
-            <div className="">
-                {children}
-            </div>
+          {children}
         </main>
+      </div>
     </div>
   );
 }
