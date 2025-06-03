86145c32241e18899d380f5cecb0f8d0c7415ef1
Derek <dberb789@gmail.com>
2025-06-03 17:04:54 +0800

    feat: add sidebar navigation configuration

---
+++ src/config/sidebar-cfg.ts
@@ -0,0 +1,29 @@
+import { SideBarItem } from '@/types/nav-types';
+
+export const sidebarNavItems: SideBarItem[] = [
+  {
+    title: 'Home',
+    href: '/home',
+    icon: ''
+  },
+  {
+    title: 'Explore',
+    href: '/explore',
+    icon: ''
+  },
+  {
+    title: 'Notifications',
+    href: '/Notifications',
+    icon: ''
+  },
+{
+    title: 'Messages',
+    href: '/Messages',
+    icon: ''
+  },
+{
+    title: 'Profile',
+    href: '/Profile',
+    icon: ''
+  },
+];
