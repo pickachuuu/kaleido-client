4be3866cf914b65ab74a680346685f26750a15cd
Derek <dberb789@gmail.com>
2025-06-03 22:15:09 +0800

    feat: enhance Sidebar layout and integrate More and Create Post buttons

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -3,12 +3,14 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { cn } from '@/lib/utils'
 import { sidebarNavItems } from "@/config/sidebar-cfg"
+import { Button } from '@/components/ui/Button'
+import { CircleEllipsis } from 'lucide-react'
+
 
 export default function Sidebar() {
     const pathname = usePathname()
-
     return (
-        <aside className="w-64 h-full bg-background p-4">
+        <aside className="w-64 h-full p-4 flex items-center justify-center">
             <nav className="space-y-2">
                 {sidebarNavItems.map((item) => {
                     const isActive = pathname === item.href
@@ -25,18 +27,31 @@ export default function Sidebar() {
                             )}
                         >
                             {item.icon && (
-                                <img 
-                                    src={item.icon} 
-                                    alt="" 
-                                    className="h-4 w-4"
-                                    aria-hidden="true"
+                                <item.icon color='#ffffff'
                                 />
                             )}
                             <span className='text-xl'>{item.title}</span>
                         </Link>
                     )
                 })}
+
+                {/* More Button */}
+
+                <div className='flex items-center gap-3 px-3 py-2 text-sm'>
+                    <CircleEllipsis color='#ffffff'/>
+                    <span className='text-xl'>More</span>
+                </div>
+                
+                {/* Post Button */}
+                <Button className="w-full my-2 font-bold text-black" size={'lg'}>
+                    Create Post
+                </Button>
+
             </nav>
         </aside>
     )
 }
+
+
+
+
