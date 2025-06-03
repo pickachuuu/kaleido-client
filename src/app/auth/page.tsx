a352c5604e5c643fb9f9a05281ecd11cedbb8064
Derek <dberb789@gmail.com>
2025-06-04 01:17:52 +0800

    refactor: restructure AuthPage layout for improved alignment

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,9 +3,14 @@ import Banner from "@/components/features/Banner"
 
 export default function AuthPage() {
     return (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-[20%] p-4 min-h-screen">
-            <Banner />
-            <AuthForm />
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 p-4 min-h-screen">
+            <div className="flex items-center justify-center">
+                <Banner />
+            </div>
+
+            <div className="flex items-center justify-center">
+                <AuthForm />
+            </div>
         </div>
     )
 }
