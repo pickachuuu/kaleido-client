62cf8d27d83a8af641a247f15cd3ba4c5ff8c3fc
Derek <dberb789@gmail.com>
2025-06-04 02:34:29 +0800

    refactor: add styling to ProfilePage component for improved layout

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -1,6 +1,6 @@
 export default function ProfilePage() {
     return (
-        <div>
+        <div className="border-x-1 h-screen max-w-3xl border-gray-700">
             Profile Page
         </div>
     )
