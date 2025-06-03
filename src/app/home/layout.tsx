c31caa9321cad5cf08267fa9eb4cc972db8d3867
Derek <dberb789@gmail.com>
2025-06-03 21:30:34 +0800

    feat: add sidebar layout in home page

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,11 +1,21 @@
+import Sidebar from "@/components/layout/Sidebar";
+
 export default function HomeLayout({
     children,
 }: {
     children: React.ReactNode;
-}){
+}) {
     return (
         <div>
-            {children}
+        <div className="h-screen flex">
+            <Sidebar />
+            <main className="flex-1 p-8 bg-background">
+                <div className="max-w-6xl mx-auto">
+                    {children}
+                </div>
+            </main>
+        </div>
+
         </div>
     )
 }
