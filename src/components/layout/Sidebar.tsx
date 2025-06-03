c4500387a0ea1b5d6c081ce173ae6bc0e3facaf8
Derek <dberb789@gmail.com>
2025-06-04 02:34:25 +0800

    refactor: replace CircleEllipsis icon with DotsThreeCircle in Sidebar component

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -4,14 +4,14 @@ import { usePathname } from 'next/navigation'
 import { cn } from '@/lib/utils'
 import { sidebarNavItems } from "@/config/sidebar-cfg"
 import { Button } from '@/components/ui/Button'
-import { CircleEllipsis } from 'lucide-react'
+import { DotsThreeCircle } from 'phosphor-react'
 
 
 export default function Sidebar() {
     const pathname = usePathname()
     return (
         <aside className="w-64 h-full p-4 flex items-center justify-center">
-            <nav className="space-y-2">
+            <nav className="space-y-2 w-48">
                 {sidebarNavItems.map((item) => {
                     const isActive = pathname === item.href
 
@@ -20,17 +20,17 @@ export default function Sidebar() {
                             key={item.href}
                             href={item.href}
                             className={cn(
-                                'flex items-center gap-3 rounded-lg px-3 py-2 text-sm transition-colors',
-                                isActive 
-                                    ? 'bg-secondary text-secondary-foreground' 
-                                    : 'text-muted-foreground hover:bg-accent hover:text-accent-foreground'
+                                'flex items-center gap-3 rounded-3xl px-3 py-2 text-sm transition-colors hover:bg-zinc-900',
+                                isActive ? 'font-semibold' : 'font-light',
                             )}
-                        >
+                                                >
                             {item.icon && (
-                                <item.icon color='#ffffff'
+                                <item.icon
+                                    size={32}
+                                    weight={isActive ? 'fill' : 'regular'}
                                 />
                             )}
-                            <span className='text-xl'>{item.title}</span>
+                            <span className='text-lg'>{item.title}</span>
                         </Link>
                     )
                 })}
@@ -38,7 +38,7 @@ export default function Sidebar() {
                 {/* More Button */}
 
                 <div className='flex items-center gap-3 px-3 py-2 text-sm'>
-                    <CircleEllipsis color='#ffffff'/>
+                    <DotsThreeCircle size={32} color='#ffffff'/>
                     <span className='text-xl'>More</span>
                 </div>
                 
