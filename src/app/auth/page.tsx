f0ba88322c41cb7f806cd64c67a4e43d6339fe6f
Derek <dberb789@gmail.com>
2025-06-05 00:22:03 +0800

    refactor: improve layout structure of AuthPage for better responsiveness

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,14 +3,19 @@ import Banner from "@/components/features/Banner"
 
 export default function AuthPage() {
     return (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 p-4 min-h-screen">
-            <div className="flex items-center justify-center">
-                <Banner />
-            </div>
+        <div className="flex items-center justify-center h-screen">
+            <div className="grid grid-cols-1 lg:grid-cols-2 p-4 w-full">
 
-            <div className="flex items-center justify-center">
-                <AuthForm />
+                <div className="flex items-center justify-start lg:justify-center">
+                    <Banner />
+                </div>
+
+                <div className="flex items-center justify-start lg:justify-center">
+                    <AuthForm />
+                </div>
+
             </div>
         </div>
+
     )
 }
