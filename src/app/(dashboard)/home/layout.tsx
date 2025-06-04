e87455725323f8a07d176d74b883480481d893eb
Derek <dberb789@gmail.com>
2025-06-04 22:38:55 +0800

    refactor: update HomeLayout structure for improved responsiveness and layout

--- src/app/(dashboard)/home/layout.tsx
+++ src/app/(dashboard)/home/layout.tsx
@@ -5,18 +5,24 @@ export default function HomeLayout({
 }) {
 
     return (
-        <div className="border-r-1 h-screen w-2xl border-zinc-700">
-            <div className="grid grid-cols-2">
-                <div className=" border-b-1 border-zinc-700 p-3 text-center hover:bg-zinc-900">
-                    For you
+        <div className="grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-4">
+            <div className="border-r-1 h-screen sm:min-w-xl w-1xl border-zinc-700 col-span-1">
+                <div className="grid grid-cols-2">
+                    <div className=" border-b-1 border-zinc-700 p-3 text-center hover:bg-zinc-900">
+                        For you
+                    </div>
+                    <div className="border-b-1 border-zinc-700 p-3 text-center hover:bg-zinc-900">
+                        Following
+                    </div>
                 </div>
-                <div className="border-b-1 border-zinc-700 p-3 text-center hover:bg-zinc-900">
-                    Following
-                </div>
+                {/* Home section */}
+                {children}
             </div>
+            {/* right sidebar */}
+            <div className="hidden lg:block col-span-1">
+            
+            </div>
+        </div>
 
-            {/* Home Page Feed */}
-            {children}
-        </div>  
     )
 }
