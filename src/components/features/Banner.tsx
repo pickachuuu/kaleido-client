a0c6838dab2a2355fdd21d93479ac3df86191fa8
Derek <dberb789@gmail.com>
2025-06-05 00:21:33 +0800

    refactor: clean up Banner component by removing unnecessary whitespace

--- src/components/features/Banner.tsx
+++ src/components/features/Banner.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image"
 
-
 export default function Banner() {
     return (
         <div className="flex items-center justify-center">
@@ -9,6 +8,8 @@ export default function Banner() {
                 alt="Banner Image"
                 width={520}
                 height={520}
+                className="w-16 h-16 lg:w-[496px] lg:h-[496px]"
+                priority
             />
         </div>
     )
