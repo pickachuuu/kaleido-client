645c859073ffc04d8c4456f107c14b5622b8e02d
Derek <dberb789@gmail.com>
2025-06-06 04:05:16 +0800

    fix: update visibility classes in Sidebar component for improved responsiveness

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -10,7 +10,7 @@ import { DotsThreeCircle, PlusCircle } from 'phosphor-react'
 export default function Sidebar() {
     const pathname = usePathname()
     return (
-        <aside className="w-16 lg:w-64 h-full p-4 flex items-center justify-center transition-all duration-300">
+        <aside className="w-16 xl:w-64 h-full p-4 flex items-center justify-center transition-all duration-300">
             <nav className="space-y-2 w-auto sm:w-48">
                 {sidebarNavItems.map((item) => {
                     const isActive = pathname === item.href
@@ -30,7 +30,7 @@ export default function Sidebar() {
                                     weight={isActive ? 'fill' : 'regular'}
                                 />
                             )}
-                            <span className="hidden lg:inline text-xl">{item.title}</span>
+                            <span className="hidden xl:inline text-xl">{item.title}</span>
                         </Link>
                     )
                 })}
@@ -38,17 +38,17 @@ export default function Sidebar() {
                 {/* More Button */}
                 <div className="flex items-center justify-center sm:justify-start gap-3 px-3 py-2 cursor-pointer hover:bg-zinc-900 rounded-3xl">
                     <DotsThreeCircle size={32} color="#ffffff"/>
-                    <span className="hidden lg:inline text-xl">More</span>
+                    <span className="hidden xl:inline text-xl">More</span>
                 </div>
                 
                 {/* Post Button */}
                 <       div className='flex justify-center'>
-                    <Button onClick={() => alert('Create Post')} className="hidden lg:block 
+                    <Button onClick={() => alert('Create Post')} className="hidden xl:block 
                     rounded-full md:rounded-3xl w-full my-2 font-bold text-black aspect-square sm:aspect-auto" size={'lg'}>
-                        <span className="hidden lg:inline text-lg">Post</span>
+                        <span className="hidden xl:inline text-lg">Post</span>
                     </Button>
 
-                    <button className='lg:hidden' onClick={() => alert('Create Post')}>
+                    <button className='xl:hidden' onClick={() => alert('Create Post')}>
                         <PlusCircle size={42} weight='fill'/>
                     </button>
                 </div>
