fc16d5e588df9049c2842ee7eeac5bf9c88ccf0e
Derek <dberb789@gmail.com>
2025-06-05 17:33:32 +0800

    feat: enhance NotificationsLayout structure for improved responsiveness and layout consistency

--- src/app/(dashboard)/notifications/layout.tsx
+++ src/app/(dashboard)/notifications/layout.tsx
@@ -2,13 +2,19 @@ export default function NotificationsLayout({
     children }: { children: React.ReactNode 
     }){
         return (
-        <div className="w-2xl border-r-1 border-zinc-700">
-            <div className="grid grid-cols-3 sticky top-0 z-10">
-                <div className="col-span-3 bg-foreground/0 backdrop-blur-md p-3">
+        <div className="grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-4">
+            <div className="border-r-1 sm:min-w-xl w-1xl border-zinc-700 col-span-1"> 
+                <div className="col-span-3 bg-foreground/0 backdrop-blur-md p-3 sticky top-0 z-10">
                     <h1 className="text-xl font-bold">Notifications</h1>
                 </div>
+                <div className="grid grid-cols-3 sticky top-0 z-10">
+                    {/* buttons here */}
+                </div>
+                {children}
             </div>
-            {children}
+            <div className="hidden lg:block col-span-1">
+                Test
+            </div>
         </div>
         )
     }
