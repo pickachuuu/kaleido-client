de45478d470e100a8e065fd1215e680aa0448ca6
Derek <dberb789@gmail.com>
2025-06-05 17:46:35 +0800

    fix: adjust layout component for consistent styling and structure

--- src/app/(dashboard)/home/layout.tsx
+++ src/app/(dashboard)/home/layout.tsx
@@ -6,7 +6,8 @@ export default function HomeLayout({
 
     return (
         <div className="grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-4">
-            <div className="border-r-1 h-screen sm:min-w-xl w-1xl border-zinc-700 col-span-1">
+            <div className="border-r-1 min-h-screen sm:min-w-xl w-1xl border-zinc-700 col-span-1"> 
+                <div className="col-span-3 bg-foreground/0 backdrop-blur-md sticky top-0 z-10">
                 <div className="grid grid-cols-2">
                     <div className=" border-b-1 border-zinc-700 p-3 text-center hover:bg-zinc-900">
                         For you
@@ -15,14 +16,15 @@ export default function HomeLayout({
                         Following
                     </div>
                 </div>
-                {/* Home section */}
+                </div>
+                <div className="grid grid-cols-3 sticky top-0 z-10">
+                    {/* buttons here */}
+                </div>
                 {children}
             </div>
-            {/* right sidebar */}
             <div className="hidden lg:block col-span-1">
                 Test
             </div>
         </div>
-
     )
-}
+}      
