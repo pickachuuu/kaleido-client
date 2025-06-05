f467dfa15709b24c1fd6b09997ea89c6bafc3b60
Derek <dberb789@gmail.com>
2025-06-06 03:26:04 +0800

    refactor: simplify ExploreLayout structure by removing unnecessary divs

--- src/app/(dashboard)/explore/layout.tsx
+++ src/app/(dashboard)/explore/layout.tsx
@@ -1,19 +1,21 @@
+import MainLayout from "@/components/layout/MainLayout";
+import StickyPanel from "@/components/layout/StickyPanel";
+import LayoutContainer from "@/components/layout/LayoutContainer";
+import TransparentHeader from "@/components/ui/TransparentHeader";
+
 export default function ExploreLayout({ children }: { children: React.ReactNode} ){
     return (
-        <div className="grid grid-cols-1 md:grid-cols-[2fr_1fr] gap-4">
-            <div className="border-r-1 min-h-screen sm:min-w-xl w-1xl border-zinc-700 col-span-1"> 
-                <div className="col-span-3 bg-foreground/0 backdrop-blur-md p-3 sticky top-0 z-10">
-                {/* Sticky Nav */}
-                    <h1 className="text-xl font-bold">Notifications</h1>
-                </div>
-                <div className="grid grid-cols-3 sticky top-0 z-10">
-                    {/* buttons here */}
-                </div>
+        <LayoutContainer>
+            <MainLayout>
+                <TransparentHeader>
+                    Explore
+                </TransparentHeader>
                 {children}
-            </div>
-            <div className="hidden lg:block col-span-1">
-                Test
-            </div>
-        </div>
+            </MainLayout>
+
+            <StickyPanel>
+                Components here
+            </StickyPanel>
+        </LayoutContainer>
     )
 }
