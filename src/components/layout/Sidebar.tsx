35a3ecf05e764cb8f7d2d7e324d77cc7e9ac66c6
Derek <dberb789@gmail.com>
2025-06-06 02:58:19 +0800

    fix: update Sidebar component for consistent responsive styling

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -5,13 +5,12 @@ import { cn } from '@/lib/utils'
 import { sidebarNavItems } from "@/config/sidebar-cfg"
 import { Button } from '@/components/ui/Button'
 import { DotsThreeCircle, PlusCircle } from 'phosphor-react'
-import { Plus } from 'lucide-react'
 
 
 export default function Sidebar() {
     const pathname = usePathname()
     return (
-        <aside className="w-16 md:w-64 h-full p-4 flex items-center justify-center transition-all duration-300">
+        <aside className="w-16 lg:w-64 h-full p-4 flex items-center justify-center transition-all duration-300">
             <nav className="space-y-2 w-auto sm:w-48">
                 {sidebarNavItems.map((item) => {
                     const isActive = pathname === item.href
@@ -31,7 +30,7 @@ export default function Sidebar() {
                                     weight={isActive ? 'fill' : 'regular'}
                                 />
                             )}
-                            <span className="hidden md:inline text-xl sm:hidden">{item.title}</span>
+                            <span className="hidden lg:inline text-xl">{item.title}</span>
                         </Link>
                     )
                 })}
@@ -39,17 +38,17 @@ export default function Sidebar() {
                 {/* More Button */}
                 <div className="flex items-center justify-center sm:justify-start gap-3 px-3 py-2 cursor-pointer hover:bg-zinc-900 rounded-3xl">
                     <DotsThreeCircle size={32} color="#ffffff"/>
-                    <span className="hidden md:inline text-xl">More</span>
+                    <span className="hidden lg:inline text-xl">More</span>
                 </div>
                 
                 {/* Post Button */}
                 <       div className='flex justify-center'>
-                    <Button className="hidden md:block 
+                    <Button className="hidden lg:block 
                     rounded-full md:rounded-3xl w-full my-2 font-bold text-black aspect-square sm:aspect-auto" size={'lg'}>
-                        <span className="hidden md:inline text-lg">Create Post</span>
+                        <span className="hidden lg:inline text-lg">Create Post</span>
                     </Button>
 
-                    <button className='md:hidden' onClick={() => alert('Create Post')}>
+                    <button className='lg:hidden' onClick={() => alert('Create Post')}>
                         <PlusCircle size={42} weight='fill'/>
                     </button>
                 </div>
