ecd56f629d780fb425e836789fdf089c32a2dfe6
Derek <dberb789@gmail.com>
2025-06-07 03:13:58 +0800

    feat: replace scroll test placeholders with NavButton components for navigation

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -1,52 +1,16 @@
+import NavButton from "@/components/ui/NavButton";
+import NavButtonContainer from "@/components/layout/NavButtonContainer";
 export default function Home(){
     return (
-        <div className="z-10">
-            <div className="py-6">
-                Scroll test
-            </div>
-            <div className="py-6">
-                Scroll test
-            </div>
-                        <div className="py-6">
-                Scroll test
-            </div>
-                        <div className="py-6">
-                Scroll test
-            </div>
-                        <div className="py-6">
-                Scroll test
-            </div>
-                        <div className="py-6">
-                Scroll test
-            </div>
-                        <div className="py-6">
-                Scroll test
-            </div>
-                        <div className="py-6">
-                Scroll test
-            </div>
-                        <div className="py-6">
-                Scroll test
-            </div>
-                        <div className="py-6">
-                Scroll test
-            </div>
-                        <div className="py-6">
-                Scroll test
-            </div>
-                        <div className="py-6">
-                Scroll test
-            </div>
-                        <div className="py-6">
-                Scroll test
-            </div>
-                        <div className="py-6">
-                Scroll test
-            </div>
-
-                        <div className="py-6">
-                Scroll test
-            </div>            
+        <div >
+            <NavButtonContainer>
+                <NavButton>
+                    For you
+                </NavButton>
+                <NavButton>
+                    Following
+                </NavButton>
+            </NavButtonContainer>
         </div>
     )
 }
