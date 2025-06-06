e7a729d763f387066c6940a15b395e59be926858
Derek <dberb789@gmail.com>
2025-06-06 19:28:52 +0800

    fix: remove sticky positioning from TransparentHeader for layout adjustments

--- src/components/ui/TransparentHeader.tsx
+++ src/components/ui/TransparentHeader.tsx
@@ -1,6 +1,6 @@
 export default function TransparentHeader({ children }: { children: React.ReactNode}){
     return (
-        <div className="col-span-3 bg-foreground/0 backdrop-blur-md p-3 sticky top-0 z-10">
+        <div className="col-span-3 bg-foreground/0 backdrop-blur-md p-3">
             <h1 className="text-xl font-bold">
                 {children}
             </h1>
