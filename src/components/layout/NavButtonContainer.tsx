5646534a4e10d12f99951dac836382a03eb1c2bb
Derek <dberb789@gmail.com>
2025-06-06 19:28:47 +0800

    feat: add NavButtonContainer component with variant support for flexible layouts

---
+++ src/components/layout/NavButtonContainer.tsx
@@ -0,0 +1,36 @@
+import { cva, type VariantProps } from 'class-variance-authority';
+import { cn } from '@/lib/utils';
+import React from 'react';
+
+const navButtonContainerVariants = cva(
+    'grid bg-foreground/0 backdrop-blur-md sticky top-0 z-10',
+    {
+        variants: {
+            variant: {
+                default: 'grid-cols-2',
+                three: 'grid-cols-3'
+            }
+        },
+        defaultVariants: {
+            variant: 'default'
+        }
+    }
+);
+
+export interface navButtonContainerVariantsProps extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof navButtonContainerVariants> {
+    children: React.ReactNode;
+}
+
+export default function NavButtonContainer({
+    children,
+    variant,
+    className,
+    ...props
+}: navButtonContainerVariantsProps ) {
+    return (
+        <div className={cn(navButtonContainerVariants({ variant, className }))} {...props}>
+            {children}
+        </div>
+    )
+}
