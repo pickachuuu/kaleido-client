093dcf85fcd6967dff9b5b33d408033bd408ab65
Derek <dberb789@gmail.com>
2025-06-07 21:54:05 +0800

    feat: add sticky container component

---
+++ src/components/ui/StickyContainer.tsx
@@ -0,0 +1,33 @@
+import { cva, type VariantProps } from 'class-variance-authority';
+import { cn } from '@/lib/utils'; 
+import React from 'react';
+
+const StickyContainerVariants = cva(
+    'sticky top-0 z-30 cursor-pointer',
+    {
+        variants: {
+            variant: {
+                solid: 'bg-background',
+                transparent: 'bg-foreground/0 backdrop-blur-xl'
+            }
+        },
+        defaultVariants: {
+            variant: 'transparent'
+        }
+    }
+)
+
+interface StickyContainerProps extends React.HTMLAttributes<HTMLDivElement>,
+  VariantProps<typeof StickyContainerVariants> {
+    children: React.ReactNode
+}
+
+
+
+export default function StickyContainer({ children, variant, className, ...props}: StickyContainerProps){
+    return (
+        <div className={cn(StickyContainerVariants({ variant, className }))} {...props}>
+            {children}
+        </div>
+    )
+}
