bfa5f115ebf569867e6a76ce055987a55dd5ac11
Derek <dberb789@gmail.com>
2025-06-07 19:39:28 +0800

    feat: add state management for active tab in Home component

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -1,8 +1,17 @@
+'use client';
+
 import NavButton from "@/components/ui/NavButton";
 import NavButtonContainer from "@/components/layout/NavButtonContainer";
+import ForYouFeed from "@/components/feeds/ForYouFeed";
+import FollowingFeed from "@/components/feeds/FollowingFeed";
+
+import { useState } from "react";
+
 export default function Home(){
+    const [activeTab, setActiveTab] = useState("following")
+
     return (
-        <div >
+        <div>
             <NavButtonContainer>
                 <NavButton>
                     For you
@@ -10,7 +19,11 @@ export default function Home(){
                 <NavButton>
                     Following
                 </NavButton>
+
+                {/* {activeTab == 'following'} */}
+                
             </NavButtonContainer>
+            Testing
         </div>
     )
 }
