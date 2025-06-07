46a3ce2b444c55923557b582a8d9afad3a9ba0ad
Derek <dberb789@gmail.com>
2025-06-07 19:37:25 +0800

    feat: add AllNotifs component for displaying notification messages

---
+++ src/components/notification/AllNotifs.tsx
@@ -0,0 +1,27 @@
+import Card from "../ui/Card"
+
+export default function AllNotifs(){
+    return (
+        <div>
+            <Card variant={"notif"} size={"notif"} className="p-2">
+                <Card.Header>
+                    Message
+                </Card.Header>
+
+                <Card.Content>
+                    Luna: Uli namo!
+                </Card.Content>
+            </Card>
+
+            <Card variant={"notif"} size={"notif"} className="p-2">
+                <Card.Header>
+                    Kaon
+                </Card.Header>
+
+                <Card.Content>
+                    Snowy: Pakaona nako
+                </Card.Content>
+            </Card>
+        </div>
+    )
+}
