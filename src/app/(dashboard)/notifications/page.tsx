10ac158d15b63518f8beb77a1b352792a84e9c2f
Derek <dberb789@gmail.com>
2025-06-07 19:39:15 +0800

    feat: restructure NotificationsPage to include header and navigation buttons

--- src/app/(dashboard)/notifications/page.tsx
+++ src/app/(dashboard)/notifications/page.tsx
@@ -1,12 +1,28 @@
+import TransparentHeader from "@/components/ui/TransparentHeader"
+import NavButtonContainer from "@/components/layout/NavButtonContainer"
+import NavButton from "@/components/ui/NavButton"
+import AllNotifs from "@/components/notification/AllNotifs"
+
 export default function NotificationsPage() {
     return (
         <div>
-            <div className="h-screen">
-                Content!
-            </div>
-            <div className="h-screen">
-                Content!
-            </div>
+            <TransparentHeader>
+                Notifications
+            </TransparentHeader>
+            <NavButtonContainer variant={"three"}>
+                <NavButton>
+                    All
+                </NavButton>
+                <NavButton>
+                    Verified
+                </NavButton>
+                <NavButton>
+                    Mentions
+                </NavButton>
+            </NavButtonContainer>
+
+            <AllNotifs/>
+
         </div>
     )
 }
