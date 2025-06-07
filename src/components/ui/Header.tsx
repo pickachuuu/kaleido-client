2102972de89564c90ec0dd4199d27ce522beefa2
Derek <dberb789@gmail.com>
2025-06-08 03:41:27 +0800

    style: update Header component padding and height

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header({ children }: { children: React.ReactNode}){
     return (
-        <div className="p-3">
+        <div className="h-13 p-1">
             <h1 className="text-xl font-bold">
                 {children}
             </h1>
