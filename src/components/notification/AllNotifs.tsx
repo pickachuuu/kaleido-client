66d9de6dae556b94d5253a1bddad0dea237c3600
Derek <dberb789@gmail.com>
2025-06-07 21:57:53 +0800

    feat: add all notifications for notifications page

--- src/components/notification/AllNotifs.tsx
+++ src/components/notification/AllNotifs.tsx
@@ -22,6 +22,86 @@ export default function AllNotifs(){
                     Snowy: Pakaona nako
                 </Card.Content>
             </Card>
+
+            <Card variant={"notif"} size={"notif"} className="p-2">
+                <Card.Header>
+                    Message
+                </Card.Header>
+
+                <Card.Content>
+                    Luna: Uli namo!
+                </Card.Content>
+            </Card>
+
+            <Card variant={"notif"} size={"notif"} className="p-2">
+                <Card.Header>
+                    Kaon
+                </Card.Header>
+
+                <Card.Content>
+                    Snowy: Pakaona nako
+                </Card.Content>
+            </Card>
+
+            <Card variant={"notif"} size={"notif"} className="p-2">
+                <Card.Header>
+                    Message
+                </Card.Header>
+
+                <Card.Content>
+                    Luna: Uli namo!
+                </Card.Content>
+            </Card>
+
+            <Card variant={"notif"} size={"notif"} className="p-2">
+                <Card.Header>
+                    Kaon
+                </Card.Header>
+
+                <Card.Content>
+                    Snowy: Pakaona nako
+                </Card.Content>
+            </Card>
+
+            <Card variant={"notif"} size={"notif"} className="p-2">
+                <Card.Header>
+                    Message
+                </Card.Header>
+
+                <Card.Content>
+                    Luna: Uli namo!
+                </Card.Content>
+            </Card>
+
+            <Card variant={"notif"} size={"notif"} className="p-2">
+                <Card.Header>
+                    Kaon
+                </Card.Header>
+
+                <Card.Content>
+                    Snowy: Pakaona nako
+                </Card.Content>
+            </Card>
+
+            <Card variant={"notif"} size={"notif"} className="p-2">
+                <Card.Header>
+                    Message
+                </Card.Header>
+
+                <Card.Content>
+                    Luna: Uli namo!
+                </Card.Content>
+            </Card>
+
+            <Card variant={"notif"} size={"notif"} className="p-2">
+                <Card.Header>
+                    Kaon
+                </Card.Header>
+
+                <Card.Content>
+                    Snowy: Pakaona nako
+                </Card.Content>
+            </Card>            
         </div>
     )
 }
