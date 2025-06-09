d6958a7f5fe886724aa5d3b50812e75330dbbc72
Derek <dberb789@gmail.com>
2025-06-09 12:00:00 +0800

    feat: import and add compoents to page

--- src/app/(dashboard)/explore/page.tsx
+++ src/app/(dashboard)/explore/page.tsx
@@ -1,6 +1,15 @@
+import StickyContainer from "@/components/ui/StickyContainer"
+import Header from "@/components/ui/Header"
+import SearchBar from "@/components/ui/SearchBar"
+
 export default function ExplorePage() {
     return (
-        <div className="">
+        <div>
+            <StickyContainer variant={"solid"}>
+                <Header variant={"bottomBorder"}>
+                    <SearchBar/>
+                </Header>
+            </StickyContainer>
         </div>
     )
 }
