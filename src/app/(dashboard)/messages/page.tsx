cf98e6325552e8a96989a2f9a4f58a29487be202
Derek <dberb789@gmail.com>
2025-06-10 17:01:45 +0800

    feat: restructure MessagesPage to include StickyContainer and enhance layout

--- src/app/(dashboard)/messages/page.tsx
+++ src/app/(dashboard)/messages/page.tsx
@@ -1,6 +1,19 @@
+import StickyContainer from "@/components/ui/StickyContainer"
+import Header from "@/components/ui/Header"
+import SearchBar from "@/components/ui/SearchBar"
+
 export default function MessagesPage() {
     return (
-        <div className="">
+        <div>
+            <StickyContainer>
+                <Header className="px-5 py-2">
+                    Messages
+                </Header>
+                <Header className="px-5 py-1">
+                    <SearchBar/>
+                </Header>
+            </StickyContainer>
+
         </div>
     )
 }
