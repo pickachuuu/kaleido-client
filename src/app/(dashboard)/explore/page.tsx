b1e4f736fcf4c78f75146445500cc58f13162d09
Derek <dberb789@gmail.com>
2025-06-10 16:30:46 +0800

    feat: update ExplorePage to include NavButtonContainer and NavButtons

--- src/app/(dashboard)/explore/page.tsx
+++ src/app/(dashboard)/explore/page.tsx
@@ -1,14 +1,23 @@
 import StickyContainer from "@/components/ui/StickyContainer"
 import Header from "@/components/ui/Header"
 import SearchBar from "@/components/ui/SearchBar"
+import NavButtonContainer from "@/components/layout/NavButtonContainer"
+import NavButton from "@/components/ui/NavButton"
 
 export default function ExplorePage() {
     return (
         <div>
             <StickyContainer variant={"solid"}>
-                <Header variant={"bottomBorder"}>
+                <Header className="px-5 py-1">
                     <SearchBar/>
                 </Header>
+                <NavButtonContainer>
+                    <NavButton>For you</NavButton>
+                    <NavButton>Trending</NavButton>
+                    <NavButton>News</NavButton>
+                    <NavButton>Sports</NavButton>
+                    <NavButton>Entertainment</NavButton>
+                </NavButtonContainer>
             </StickyContainer>
         </div>
     )
