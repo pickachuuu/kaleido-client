e41aceeb27c362d06c298b78d76e6b660027ae05
Derek <dberb789@gmail.com>
2025-06-10 17:01:42 +0800

    fix: remove TransparentHeader from MessagesLayout and update StickyPanel content

--- src/app/(dashboard)/messages/layout.tsx
+++ src/app/(dashboard)/messages/layout.tsx
@@ -1,22 +1,17 @@
 import MainLayout from "@/components/layout/MainLayout";
 import StickyPanel from "@/components/layout/StickyPanel";
 import LayoutContainer from "@/components/layout/LayoutContainer";
-import TransparentHeader from "@/components/ui/Header";
 
 export default function MessagesLayout({ children }: { children: React.ReactNode }) {
     return (
         <LayoutContainer variant={"message"}>
             <MainLayout variant={"message"}>
-                <TransparentHeader>
-                    Messages
-                </TransparentHeader>
                 {children}
             </MainLayout>
 
             <StickyPanel variant={"message"}>
-                Components here
+                Messages Here
             </StickyPanel>
-
         </LayoutContainer>
     );
 }
