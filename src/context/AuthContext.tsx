e9ed41d0932a60188b8e2e38ac9f04b2ad1b2b7d
Derek <dberb789@gmail.com>
2025-06-12 01:11:26 +0800

    feat: implement AuthContext for user authentication state management

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,19 @@
+import { createContext, useState } from "react";
+import { ProfileType } from "@/types/profileTypes";
+import { AuthContextType, AuthState } from "@/types/authContextType";
+
+const AuthContext = createContext<AuthContextType | null>(null);
+const initialAuthState: AuthState = {
+  isLoggedIn: false,
+  user: {
+    username: "",
+    email: "",
+    profileImage: "",
+    bannerUrl: "",
+    followerCount: 0,
+    followingCount: 0,
+    createdAt: ""
+},
+  isLoading: false,
+  isError: false,
+};
