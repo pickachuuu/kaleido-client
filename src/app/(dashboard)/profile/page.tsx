2bb758e4eba1ee78e075ed3631a8ef43a710c42f
Derek <dberb789@gmail.com>
2025-06-11 21:26:28 +0800

    feat: enhance ProfilePage layout with user details and join date

--- src/app/(dashboard)/profile/page.tsx
+++ src/app/(dashboard)/profile/page.tsx
@@ -1,8 +1,10 @@
+'use client';
 import Header from "@/components/ui/Header"
 import ProfileBanner from "@/components/ui/ProfileBanner"
 import ProfileAvatar from "@/components/ui/ProfileAvatar"
 import StickyContainer from "@/components/ui/StickyContainer"
 import { Button } from "@/components/ui/Button"
+import { CalendarPlus } from "phosphor-react"
 
 export default function ProfilePage() {
     return (
@@ -18,11 +20,13 @@ export default function ProfilePage() {
                 <ProfileAvatar/>
                 <Button variant={"outline"} size={"sm"}>Edit Profile</Button>
             </div>
-            <div className="flex-row w-46">
-                <div>Pika</div>
-                <div>@ZygPika</div>
-                <div>Joined May 9 2018</div>
-                <div className="flex">
+            <div className="flex-row w-46 m-3">
+                <div className="font-bold">Pika</div>
+                <div className="mb-3 text-zinc-500">@ZygPika</div>
+                <div className="flex item-center my-3 text-zinc-500">
+                    <CalendarPlus size={"20"} className="mr-2"/>  Joined October 2021
+                </div>
+                <div className="flex my-3 text-zinc-500">
                     <div className="flex-1">
                         Following
                     </div>
@@ -31,6 +35,8 @@ export default function ProfilePage() {
                     </div>
                 </div>
             </div>
+
+
         </div>
     )
 }
