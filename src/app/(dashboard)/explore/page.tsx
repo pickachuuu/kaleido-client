e357c203a58f73e739ec2778f20cf98bde50eb91
Derek <dberb789@gmail.com>
2025-06-15 23:06:52 +0800

    feat: implement tab navigation state in ExplorePage component

--- src/app/(dashboard)/explore/page.tsx
+++ src/app/(dashboard)/explore/page.tsx
@@ -1,10 +1,14 @@
+'use client'
 import StickyContainer from "@/components/ui/StickyContainer"
 import Header from "@/components/ui/Header"
 import SearchBar from "@/components/ui/SearchBar"
 import NavButtonContainer from "@/components/layout/NavButtonContainer"
 import NavButton from "@/components/ui/NavButton"
+import { useState } from "react"
 
 export default function ExplorePage() {
+    const [activeTab, setActiveTab] = useState<'For you'| 'Trending' | 'News' | 'Sports'| 'Entertainment' >('For you')
+
     return (
         <div>
             <StickyContainer variant={"solid"}>
@@ -12,11 +16,11 @@ export default function ExplorePage() {
                     <SearchBar/>
                 </Header>
                 <NavButtonContainer>
-                    <NavButton>For you</NavButton>
-                    <NavButton>Trending</NavButton>
-                    <NavButton>News</NavButton>
-                    <NavButton>Sports</NavButton>
-                    <NavButton>Entertainment</NavButton>
+                    <NavButton active={activeTab === 'For you'}>For you</NavButton>
+                    <NavButton active={activeTab === 'Trending'}>Trending</NavButton>
+                    <NavButton active={activeTab === 'News'}>News</NavButton>
+                    <NavButton active={activeTab === 'Sports'}>Sports</NavButton>
+                    <NavButton active={activeTab === 'Entertainment'}>Entertainment</NavButton>
                 </NavButtonContainer>
             </StickyContainer>
         </div>
