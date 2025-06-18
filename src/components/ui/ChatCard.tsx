ea86e6c7c4f680837395935a0d1f66ca0958eaae
Derek <dberb789@gmail.com>
2025-06-18 21:38:08 +0800

    fix: update ChatCard component styles and props for improved layout and flexibility

--- src/components/ui/ChatCard.tsx
+++ src/components/ui/ChatCard.tsx
@@ -2,27 +2,49 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const ChatCardVariants = cva(
-    'bg-background w-full min-h-36',
+    'bg-background w-full min-h-24 p-3',
     {
         variants: {
             variant: { 
                 default: 'hover:bg-zinc-700'
             }
+        },
+        defaultVariants: {
+            variant: 'default'
         }
     }
 )
 
-interface ChatCardtype extends React.HtmlHTMLAttributes<HTMLDivElement>,
+interface ChatCardProps extends React.HtmlHTMLAttributes<HTMLDivElement>,
     VariantProps<typeof ChatCardVariants>{
         children: React.ReactNode
-        className: string
+        className?: string
         active: boolean
     }
 
-export default function ChatCard({ children, active, className, variant, ...props }: ChatCardtype){
+export default function ChatCard({ children, active, className, variant, ...props }: ChatCardProps){
     return (
         <div className={cn(ChatCardVariants({ className, variant }))} {...props}>
             {children}
+        </div>
+    )
+}
+
+ChatCard.Content = function ChatCardHeader({ children, active, className, variant, ...props}: ChatCardProps){
+    return (
+        <div>
+            <div className='grid grid-cols-5'>
+                <div className='col-span-2'>
+                    Profile
+                </div>
+
+                <div className='col-span-3'>
+                    Icon
+                </div>
+            </div>
+            <div>
+                test test
+            </div>
         </div>
     )
 }
