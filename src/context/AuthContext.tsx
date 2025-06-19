f7d5a1a5c0075bf39aa7bc0d6261cad7d49b2a3b
Derek <dberb789@gmail.com>
2025-06-19 16:36:13 +0800

    fix: remove initialAuthState definition from AuthContext for cleaner code

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -3,17 +3,4 @@ import { ProfileType } from "@/types/profile-types";
 import { AuthContextType, AuthState } from "@/types/authContextType";
 
 const AuthContext = createContext<AuthContextType | null>(null);
-const initialAuthState: AuthState = {
-  isLoggedIn: false,
-  user: {
-    username: "",
-    email: "",
-    profileImage: "",
-    bannerUrl: "",
-    followerCount: 0,
-    followingCount: 0,
-    createdAt: ""
-},
-  isLoading: false,
-  isError: false,
-};
+
