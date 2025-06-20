422eb44ac5a088ba15748f1bbbe6009ad7f0336f
Derek <dberb789@gmail.com>
2025-06-20 21:09:19 +0800

    fix: add 'use client' directive to ChatContext for proper client-side rendering

--- src/context/ChatContext.tsx
+++ src/context/ChatContext.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { createContext, useState, useContext } from "react";
 import { ChatContextType } from "@/types/ChatContextType";
 import { messageType } from "@/types/message-types";
