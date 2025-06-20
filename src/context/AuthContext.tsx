d6f79627950a86b50efe6f863ad08c87c81f5bf7
Derek <dberb789@gmail.com>
2025-06-20 21:08:59 +0800

    feat: implement AuthContext and AuthProvider for authentication state management

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,6 +1,81 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useReducer } from "react";
 import { ProfileType } from "@/types/profile-types";
 import { AuthContextType, AuthState } from "@/types/authContextType";
+import { dummyProfile } from "@/lib/dummyUser";
+
+
+const initialState: AuthState = {
+    isLoading: false,
+    isLoggedIn: false,
+    isError: false,
+    user: null,
+}
+
+type Action = 
+    | {type: "LOGIN", payload: ProfileType }
+    | {type: "LOGOUT" }
+    | {type: "SET_LOADING", payload: boolean }
+    | {type: "SET_ERROR", payload: boolean };
+
+const authReducer = (state: AuthState, action: Action): AuthState => {
+    switch (action.type){
+        case 'LOGIN':
+            return {
+                ...state,
+                user: action.payload,
+                isLoggedIn: true,
+                isLoading: false,
+                isError: false
+            };
+        case 'LOGOUT':
+            return {
+                ...state,
+                user: null,
+                isLoggedIn: false,
+                isLoading: false
+            };
+        case 'SET_ERROR':
+            return {
+                ...state,
+                isError: true
+            };
+        case 'SET_LOADING':
+            return {
+                ...state,
+                isLoading: true
+            };
+        default:
+            return state;
+    }
+}
 
 const AuthContext = createContext<AuthContextType | null>(null);
 
+
+export const AuthProvider = ({children}:{children: React.ReactNode}) => {
+    const [state, dispatch] = useReducer(authReducer, initialState);
+
+    const login = (userData: ProfileType ) => {
+        dispatch({type: "SET_LOADING", payload: true})
+        dispatch({type: "LOGIN", payload: dummyProfile})
+    };
+
+    const logout = () => {
+        dispatch({type: "SET_LOADING", payload: true})
+        dispatch({type: "LOGOUT"})
+    }
+
+    return (
+        <AuthContext.Provider value={{...state, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+    if (!context) throw new Error("useAuth must be used inside the AuthProvider");
+    return (
+        context
+    )
+}
