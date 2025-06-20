20b768631376a1c52b02452f93f733ff6a7572fc
Derek <dberb789@gmail.com>
2025-06-20 21:09:13 +0800

    fix: update user type in AuthState and AuthContextType to allow null values

--- src/types/authContextType.tsx
+++ src/types/authContextType.tsx
@@ -2,14 +2,14 @@ import { ProfileType } from "./profile-types";
 
 export type AuthState = {
   isLoggedIn: boolean;
-  user: ProfileType;
+  user: ProfileType | null;
   isLoading: boolean;
   isError: boolean;
 }
 
 export interface AuthContextType {
   isLoggedIn: boolean;
-  user: ProfileType;
+  user: ProfileType | null;
   isLoading: boolean;
   isError: boolean;
   login: (userData: ProfileType) => void;
