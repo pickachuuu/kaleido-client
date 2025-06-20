de39774c84073d443fc0c30c82b223d9d9b87512
Derek <dberb789@gmail.com>
2025-06-20 12:03:43 +0800

    feat: implement ChatContext and chatProvider for managing chat state

---
+++ src/context/ChatContext.tsx
@@ -0,0 +1,24 @@
+import { createContext, useState, useContext } from "react";
+import { ChatContextType } from "@/types/ChatContextType";
+import { messageType } from "@/types/message-types";
+
+export const ChatContext = createContext<ChatContextType | undefined >(undefined);
+
+export const chatProvider = ({children}:{children: React.ReactNode}) => {
+    const [selectedConversationId, setSelectedConversationId] = useState<string | null>(null)
+    const [messages, setMessages] = useState<messageType[]>([])
+
+    return (
+        <ChatContext.Provider value={{selectedConversationId, setSelectedConversationId, messages, setMessages}}>
+            {children}
+        </ChatContext.Provider>
+    )
+}
+
+export const useChat = () => {
+    const context = useContext(ChatContext)
+    if (!context) throw new Error("useChat must be used inside a chatProvider");
+    return context; 
+}
+
+
