3265880dcbe793e24b9bd4e5933b56d062e6fa92
Derek <dberb789@gmail.com>
2025-07-08 17:00:47 +0800

    refactor: remove AuthProvider from RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
+import React from "react";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { AuthProvider } from "@/context/AuthContext";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -28,9 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>
           {children}
-        </AuthProvider>
       </body>
     </html>
   );
