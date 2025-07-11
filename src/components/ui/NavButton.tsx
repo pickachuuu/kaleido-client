1d0100f003a91dcde641f5ce864b423614e7c142
Derek <dberb789@gmail.com>
2025-07-11 23:43:15 +0800

    style: update NavButton component styles to use CSS variables for colors and enhance active state indication

--- src/components/ui/NavButton.tsx
+++ src/components/ui/NavButton.tsx
@@ -2,7 +2,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const navButtonVariants = cva(
-    'flex-1 font-bold text-zinc-500 text-center p-3 border-b-1 border-zinc-700 hover:bg-zinc-900 sticky top-0 h-13 cursor-pointer',
+    'flex-1 font-bold text-[var(--accent-secondary)] text-center p-3 border-b border-[var(--accent-secondary)] hover:bg-[var(--highlight)] sticky top-0 h-13 cursor-pointer',
     {
         variants:{
             variant:{
@@ -22,11 +22,11 @@ export interface NavButtonProps extends React.HTMLAttributes<HTMLDivElement>,
 
 export default function NavButton({ children, active, variant, className, ...props }: NavButtonProps) {
     return (
-        <div className={cn(navButtonVariants({ variant, className}), active && 'text-foreground')} {...props}>
+        <div className={cn(navButtonVariants({ variant, className}), active && 'text-[var(--accent)]')} {...props}>
             {children}
 
             <div className='flex justify-center items-center'>
-                <span className={cn('absolute  bottom-[0.5px] h-1 rounded-xl bg-blue-600 duration-500 text-center', active && 'w-16')}></span>
+                <span className={cn('absolute bottom-[0.5px] h-1 rounded-xl bg-[var(--accent)] duration-500 text-center', active && 'w-16')}></span>
             </div>
         </div>
     )
