53fdc332d5a8cd6a76b0aae78529304a2574d2f1
Derek <dberb789@gmail.com>
2025-07-14 15:26:30 +0800

    refactor: replace TransparentHeader with Header component in NotificationsPage for improved styling and consistency

--- src/app/(dashboard)/notifications/page.tsx
+++ src/app/(dashboard)/notifications/page.tsx
@@ -1,61 +1,57 @@
 'use client';
 
 import { useState } from "react";
-import TransparentHeader from "@/components/ui/Header";
 import NavButtonContainer from "@/components/layout/NavButtonContainer";
 import NavButton from "@/components/ui/NavButton";
 import AllNotifs from "@/components/notification/AllNotifs";
 import VerifiedNotifs from "@/components/notification/VerifiedNotifs";
 import MentionNotifs from "@/components/notification/MentionNotifs";
-import ScrollableContent from "@/components/ui/ScrollableContent";
 import StickyContainer from "@/components/ui/StickyContainer";
+import Header from "@/components/ui/Header";
 
 export default function NotificationsPage() {
     const [activeTab, setActiveTab] = useState<'all' | 'verified' | 'mentions'>('all');
 
     return (
         <div>
-
-                {/* Header Section   */}
-                <StickyContainer>   
-                    <TransparentHeader>
-                        Notifications
-                    </TransparentHeader>
-                    <NavButtonContainer variant={"three"}>
-                        <NavButton 
-                        onClick={()=>{setActiveTab('all')}}
-                        active={activeTab === 'all'}>
-                            All
-                        </NavButton>
-                        <NavButton 
-                        onClick={()=>{setActiveTab('verified')}}
-                        active={activeTab === 'verified'}>
-                            Verified
-                        </NavButton>
-                        <NavButton 
-                        onClick={()=>{setActiveTab('mentions')}}
-                        active={activeTab === 'mentions'}>
-                            Mentions
-                        </NavButton>
-                    </NavButtonContainer>
-                </StickyContainer>
-
-                {/* Notifs */}
-                <div>
-                    {(() => { 
-                        switch(activeTab){
-                            case 'all':
-                                return <AllNotifs/>
-                            case 'verified':
-                                return <VerifiedNotifs/>
-                            case 'mentions':
-                                return <MentionNotifs/> 
-                            default:
-                                return <AllNotifs/>
-                        }
-                    })()}
-                </div>
-
+            {/* Header Section   */}
+            <StickyContainer>
+                <Header className="px-5 py-2">
+                Notifications
+                </Header>
+                <NavButtonContainer variant={"three"}>
+                    <NavButton 
+                    onClick={()=>{setActiveTab('all')}}
+                    active={activeTab === 'all'}>
+                        All
+                    </NavButton>
+                    <NavButton 
+                    onClick={()=>{setActiveTab('verified')}}
+                    active={activeTab === 'verified'}>
+                        Verified
+                    </NavButton>
+                    <NavButton 
+                    onClick={()=>{setActiveTab('mentions')}}
+                    active={activeTab === 'mentions'}>
+                        Mentions
+                    </NavButton>
+                </NavButtonContainer>
+            </StickyContainer>
+            {/* Notifs */}
+            <div>
+                {(() => { 
+                    switch(activeTab){
+                        case 'all':
+                            return <AllNotifs/>
+                        case 'verified':
+                            return <VerifiedNotifs/>
+                        case 'mentions':
+                            return <MentionNotifs/> 
+                        default:
+                            return <AllNotifs/>
+                    }
+                })()}
+            </div>
         </div>
     )
 }
